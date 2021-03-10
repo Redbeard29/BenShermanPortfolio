0c97c93800e088341095a590409a258c2c07d5aa
Redbeard29 <ben@benshermanmusic.com>
2021-03-10 13:20:05 -0500

    added jQuery animations for fadeIn

--- script.js
+++ script.js
@@ -1,33 +1,23 @@
-/*$(document).ready(function(){
-    $(window).scroll(function(){
-
-    })
-    //dynamically appearing elements on scroll
-    $("#see_my_work_button").click(function(){
-        for(let x = 0; x < 5; x++){
-            $("#attribute_card_" + x).fadeIn("slow");
-        }
-    });
-});
-*/
-
 $(document).ready(function() {
-
+    
     /* Every time the window is scrolled ... */
-    $(window).scroll(function(){
+    $(window).scroll( function(){
     
         /* Check the location of each desired element */
-        $("#hidden_element").each(function(){
-    
-            const bottom_of_object = $(this).position().top + $(this).outerHeight();
-            const bottom_of_window = $(window).scrollTop() + $(window).height();
-    
-            /* If the object is completely visible in the window, fade it in */
+        $('.hidden_element').each(function(i){
+            
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            /* If the object is completely visible in the window, fade it it */
             if( bottom_of_window > bottom_of_object ){
-    
-                $(this).animate({'opacity':'1'},500);
-    
-            }    
+                
+                $(this).animate({'opacity':'1'},750);
+                    
+            }
+            
         }); 
-    }); 
+    
+    });
+    
 });
