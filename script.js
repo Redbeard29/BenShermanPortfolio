caf425a29750797fedf278b4f4874c9234d5a5bd
Redbeard29 <ben@benshermanmusic.com>
2021-03-11 16:47:44 -0500

    added jQuery to change color class on highlighted portfolio section

--- script.js
+++ script.js
@@ -25,41 +25,52 @@ $(document).ready(function() {
         $('html, body').animate({
             scrollTop: $("#portfolio_section").offset().top}, 2000);
     });
+
     //View projects button scroll
     $("#view_projects_button").click(function(){
         $('html, body').animate({
             scrollTop: $("#portfolio_section").offset().top}, 2000);
     });
+
     //Let's connect button scroll
     $("#lets_connect_button").click(function(){
         $('html, body').animate({
             scrollTop: $("#contact_section").offset().top}, 2000);
     });
+
     //About nav link scroll
     $("#about_nav_link").click(function(){
         $('html, body').animate({
             scrollTop: $("#about_section").offset().top}, 2000);
     });
+
     //Portfolio nav link scroll
     $("#portfolio_nav_link").click(function(){
         $('html, body').animate({
             scrollTop: $("#portfolio_section").offset().top}, 2000);
     });
-    //resume nav link scroll
+
+    //Resume nav link scroll
     $("#resume_nav_link").click(function(){
         $('html, body').animate({
             scrollTop: $("#resume_section").offset().top}, 2000);
     });
+
     //Contact nav link scroll
     $("#contact_nav_link").click(function(){
         $('html, body').animate({
             scrollTop: $("#contact_section").offset().top}, 2000);
     });
 
+
     /* Filtering by language */
 
     //Click portfolio container items
     $('#portfolio_container h5').click(function(){
+
+        $("#portfolio_container h5").removeClass("text-danger").addClass("text-white");
+
+        $(this).addClass("text-danger");
 
         var language = $(this).attr('data-filter');
         
