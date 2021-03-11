d27917c8d4d271c58306528307965d79169a827a
Redbeard29 <ben@benshermanmusic.com>
2021-03-10 22:13:23 -0500

    used jQuery to implement button clicks that scroll to corresponding page sections, linked resume download button to google doc of resume

--- script.js
+++ script.js
@@ -9,15 +9,48 @@ $(document).ready(function() {
             var bottom_of_object = $(this).offset().top + $(this).outerHeight();
             var bottom_of_window = $(window).scrollTop() + $(window).height();
             
-            /* If the object is completely visible in the window, fade it it */
+            /* If the object is completely visible in the window, fade it in */
             if( bottom_of_window > bottom_of_object ){
                 
-                $(this).animate({'opacity':'1'},750);
+                $(this).animate({'opacity':'1'},700);
                     
             }
-            
+      
         }); 
-    
     });
-    
+    //See my work button scroll 
+    $("#see_my_work_button").click(function(){
+        $('html, body').animate({
+            scrollTop: $("#portfolio_section").offset().top}, 2000);
+    });
+    //View projects button scroll
+    $("#view_projects_button").click(function(){
+        $('html, body').animate({
+            scrollTop: $("#portfolio_section").offset().top}, 2000);
+    });
+    //Let's connect button scroll
+    $("#lets_connect_button").click(function(){
+        $('html, body').animate({
+            scrollTop: $("#contact_section").offset().top}, 2000);
+    });
+    //About nav link scroll
+    $("#about_nav_link").click(function(){
+        $('html, body').animate({
+            scrollTop: $("#about_section").offset().top}, 2000);
+    });
+    //Portfolio nav link scroll
+    $("#portfolio_nav_link").click(function(){
+        $('html, body').animate({
+            scrollTop: $("#portfolio_section").offset().top}, 2000);
+    });
+    //resume nav link scroll
+    $("#resume_nav_link").click(function(){
+        $('html, body').animate({
+            scrollTop: $("#resume_section").offset().top}, 2000);
+    });
+    //Contact nav link scroll
+    $("#contact_nav_link").click(function(){
+        $('html, body').animate({
+            scrollTop: $("#contact_section").offset().top}, 2000);
+    });
 });
